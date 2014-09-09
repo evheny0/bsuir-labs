61aab7619a311463ff8d223feb6e00d01747f0ed
evheny0 <evheny0@gmail.com>
2014-09-09 20:43:01 +0300

    Working first lab

--- spovm/lab1/child.c
+++ spovm/lab1/child.c
@@ -1,15 +1,42 @@
-#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <ncurses.h>
 
-#define BUF_SIZE 40
+
+int compare(const void *a, const void *b)
+{
+    return (*((char *)a) - *((char *)b));
+}
+
+void check_params(char const *argv[])
+{
+    if (!argv[1]) {
+        puts("No input string\n");
+        exit(1);
+    }
+}
+
+void string_to_file(char *str, FILE *file)
+{
+    fprintf(file, "%s", str);
+}
+
+
 
 int main(int argc, char const *argv[])
 {
-    int i;
-    //execl("child", NULL);
-    printf("PID child exec: %d\n", getpid());
+    printf("*** PID: %d\n", getpid());
+    check_params(argv);
 
+    char *str = calloc(strlen(argv[1]), sizeof(argv[1][0]));
+    FILE *file = fopen("output", "w");
 
+    strcpy(str, argv[1]);
+    qsort(str, strlen(argv[1]), sizeof(*argv[1]), compare);
+
+    string_to_file(str, file);
+    fclose(file);
     return 0;
 }
--- spovm/lab1/main.c
+++ spovm/lab1/main.c
@@ -1,41 +1,55 @@
-// #include <sys/types.h>
-// #include <stdio.h>
-// #include <ncurses.h>
-
-// #define BUF_SIZE 40
-
-// int main(int argc, char const *argv[])
-// {
-//     pid_t pid = 0;
-//     int i;
-//     char buf[BUF_SIZE];
-
-//     pid = fork();
-//     if (!pid) {
-//         sprintf(buf, "This is process %d", pid);
-//         puts(buf);
-//     } else {
-//         waitpid(pid);
-//     }
-//     return 0;
-// }
-
-#include <sys/types.h>
+//#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 #include <ncurses.h>
+#include <sys/wait.h>
 
-#define BUF_SIZE 40
+const char *PATH_TO_CHILD = "child";
+const int MAX_STRING_SIZE = 100;
+
+
+void create_child_process(char *string)
+{
+    execl(PATH_TO_CHILD, PATH_TO_CHILD, string, NULL);
+}
+
+int wait_child_process(int pid)
+{
+    int status;
+    waitpid(pid, &status, 0);
+    printf("*** PID: %d\n", getpid());
+
+    if (status) {
+        puts("Some error ocured\n");
+    } else {
+        puts("Success!\n");
+    }
+    return status;
+}
+
+char *cut_string(char *str)
+{
+    str[strlen(str) - 1] = 0;
+    return str;
+}
+
+
 
 int main(int argc, char const *argv[])
 {
+    printf("*** PID: %d\n", getpid());
+    char buffer[MAX_STRING_SIZE];
     pid_t pid = 0;
-    int i;
+
+    fgets(buffer, MAX_STRING_SIZE, stdin);
+    cut_string(buffer);
     pid = fork();
+
     if (!pid) {
-        execl("/home/eugene0/code/bsuir/spovm/lab1/child", "/home/eugene0/code/bsuir/spovm/lab1/child", "lalal0", NULL);
+        create_child_process(buffer);
     }
-    waitpid(pid);
-    printf("PID: %d\n", getpid());
-    printf("child PID: %d\n", pid);
+    wait_child_process(pid);
+
     return 0;
 }
