34e95b35e2f4cc66144711414a95df0725bb85e1
evheny0 <evheny0@gmail.com>
2014-10-06 23:55:34 +0300

    spovm2: string output now in child process

--- spovm/lab2/main.c
+++ spovm/lab2/main.c
@@ -8,34 +8,24 @@
 #include <stdlib.h>
 
 const int MAX_PIDS = 100;
-const char str[] = "Current PID is: ";
+const char *PATH_TO_CHILD = "child";
 
-void for_child_process(int semaphore_id)
+typedef int sem_t;
+
+
+char *sem_to_string(sem_t semaphore_id)
 {
-    struct sembuf dec, inc, wait;
-
-    dec.sem_num = inc.sem_num = wait.sem_num = 0;
-    dec.sem_op = -1;
-    inc.sem_op = 1;
-    wait.sem_op = 0;
-
-    while (1) {
-        semop(semaphore_id, &wait, 1);
-        semop(semaphore_id, &inc, 1);
-        for (int i = 0; i < strlen(str); i++) {
-            printf("%c", str[i]);
-        }
-        printf("%d\n", getpid());
-        semop(semaphore_id, &dec, 1);
-    }
+    char *str = calloc(100, sizeof(char));
+    sprintf(str, "%d", (int)semaphore_id);
+    return str;
 }
 
-int create_process(int semaphore_id)
+int create_process(sem_t semaphore_id)
 {
     pid_t pid = 0;
     pid = fork();
     if (!pid) {
-        for_child_process(semaphore_id);
+        execl(PATH_TO_CHILD, PATH_TO_CHILD, sem_to_string(semaphore_id), NULL);
     }
     return pid;
 }
@@ -53,7 +43,7 @@ void kill_childs(int *pids, int pid_counter)
     }
 }
 
-void main_loop(int semaphore_id)
+void main_loop(sem_t semaphore_id)
 {
     int end = 0, pid_counter = 0, pids[MAX_PIDS];
 
@@ -84,7 +74,7 @@ int create_semaphore(const char* path)
 
 int main(int argc, char const *argv[])
 {
-    int semaphore_id = create_semaphore(argv[0]);
+    sem_t semaphore_id = create_semaphore(argv[0]);
     main_loop(semaphore_id);
 
     return 0;
