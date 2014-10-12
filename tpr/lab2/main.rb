fcba107dddc2e4d7ac5995a0a95b0965d93023e2
evheny0 <evheny0@gmail.com>
2014-10-12 22:49:25 +0300

    tpr2: completed. Need to format output

--- tpr/lab2/main.rb
+++ tpr/lab2/main.rb
@@ -1,76 +1,219 @@
+#!/usr/bin/env ruby
 require "matrix"
 require "../lib/iris_reader.rb"
+require 'pry'
+
 
 T = 0.5  # threshold
-LEARNING_RATE = 0.01  # alpha
-START_WEIGHT = [0.5, 0.5, 0.5, 0.5]  # W0
-START_OUTPUT = { "Iris-setosa" => 0, "Iris-versicolor" => 1 }  # D
+START_WEIGHT = [0.3, 0.3, 0.3, 0.3]  # W0, not used
+TRUE_OUTPUT = { "Iris-setosa" => [1, 0, 0], "Iris-versicolor" => [0, 1, 0], "Iris-virginica" => [0, 0, 1] }  # D
+
+
+LEARNING_SET_START = 0
+LEARNING_SET_END = 50
+LEARNING_TRESHOLD = 0.95
+ETA = 0.5 # learning coefficient
+# learning treshold is 91%:
+# 1 => 1
+# 0.5 => 1..2
+# 0.1 => 2..27
+# 0.5 => 2..42
+# 0.05 => 2..99
+# 0.005 => 37..5720
+# 0.0005 => 126119
+
+def rand_weight
+  [rand, rand, rand, rand]
+end
+
 
 class Neuron
-  def initialize w, threshold
-    @weight = w
-    @threshold = threshold
+  def initialize
+    @weight = rand_weight
+    @threshold = T
   end
 
-  def get_weighted_sum input_elems
-    (Vector.elements(input_elems) + Vector.elements(@weight)).inject(:+) - @threshold
+  def weighted_sum input_elems
+    Vector.elements(input_elems).inner_product(Vector.elements(@weight)) - @threshold
   end
 
-  def get_y input
+  def activation_function input
     1 / (1 + Math.exp(-input))
   end
 
-  def set_w n, value
-    @weight[n] = value
+  def weight
+    @weight
   end
 
-  def recalculate_w y, values, type
-    (0..3).each do |n|
-      @weight[n] = @weight[n] + (LEARNING_RATE * values[n] * (START_OUTPUT[type] - y.round))
+  def recalculate_weight input, delta, output
+    @weight.map!.with_index do |w, i|
+      w += ETA * delta * output * (1 - output) * input[i] # delta_w_i = eta * delta * df(e)/de * x_i
+      # p ETA * delta * output * (1 - output) * input[i]
+
     end
   end
 
-  def get_w n
-    @weight[n]
-  end
-
-  def type_correct? type, y
-    y.round == START_OUTPUT[type]
-  end
-
-  def learn_by set
-    set.each do |type, values|
-      next if type == "Iris-virginica"
-      values.each do |value|
-        y = get_y(get_weighted_sum(value))
-        recalculate_w(y, value, type)
-      end
-    end
-  end
-
-  def print_results set
-    i = 0
-    set.each do |type, values|
-      next if type == "Iris-virginica"
-      values.each do |value|
-        y = get_y(get_weighted_sum(value))
-        printf(" %s #", (type_correct?(type, y)) ? "1" : "\033[1;31m0\033[0m")
-        printf("result: %2.2f # %s # %15s # ", y, value.to_s, type)
-        printf("%s %f # \n", @weight.to_s, @threshold)
-        i += 1 if type_correct?(type, y)
-      end
-    end
-    printf("Recognotion ability is: %d%\n", i)
+  def get_y input_elems
+    activation_function(weighted_sum(input_elems))
   end
 end
 
-n = Neuron.new START_WEIGHT, T
+
+
+class NeuronLayer < Array
+  def calculate_output value
+    @input = value
+    @output = []
+    each do |neuron|
+      @output << neuron.get_y(value)
+    end
+    @output
+  end
+
+  def calculate_delta prev_layer_delta, prev_layer_weight
+    @delta = []
+    count.times do |i|
+      @delta << Vector.elements(prev_layer_delta).inner_product(Vector.elements(prev_layer_weight[i]))
+    end
+    @delta
+  end
+
+  def delta=(value)
+    @delta = value
+  end
+
+  def recalculate_weights
+    each_with_index do |neuron, i|
+      neuron.recalculate_weight(@input, @delta[i], @output[i])
+    end
+  end
+
+  def print
+    puts ' ############## ' * count
+    4.times do |i|
+      each do |neuron|
+        printf(" # \033[1;30mw%d:\033[0m %+2.3f # ", i + 1, neuron.weight[i])
+      end
+      printf "\n"
+    end
+    puts ' ############## ' * count
+
+    @output.each do |value|
+      printf("      %2.3f     ", value)
+    end
+    printf "\n"
+  end
+
+  def weights
+    input_num.times.map { |i| get_weights_of_input i }
+  end
+
+  private
+
+  def get_weights_of_input n
+    map { |neuron| neuron.weight[n] }
+  end
+
+  def input_num
+    num ||= last.weight.count
+  end
+end
+
+
+
+class Perceptron
+  def initialize n_hidden_layer, n_output_layer
+    @hidden_layer = NeuronLayer.new(n_hidden_layer) { Neuron.new }
+    @output_layer = NeuronLayer.new(n_output_layer) { Neuron.new }
+  end
+
+  def learn_by_set set
+    correct_otput_count = 0
+    set.each do |type, values|
+      values[LEARNING_SET_START..LEARNING_SET_END].each do |value|
+        calculate_output(type, value)
+        calculate_deltas(type)
+
+        # print_net value, type
+
+        recalculate_weight
+        correct_otput_count += check_is_output_correct type
+      end
+    end
+    print_net nil, nil
+    correct_otput_count
+  end
+
+  def calculate_output type, value
+    hidden_y = @hidden_layer.calculate_output value
+    output_y = @output_layer.calculate_output hidden_y
+    @output_y = round_values output_y
+  end
+
+  def calculate_deltas type
+    result_deltas = calculate_output_delta(@output_y, type)
+    @output_layer.delta = result_deltas
+    @hidden_layer.calculate_delta(result_deltas, @output_layer.weights)
+  end
+
+  def recalculate_weight
+    @output_layer.recalculate_weights
+    @hidden_layer.recalculate_weights
+  end
+
+  def print_net input, type
+    input.each do |value|
+      printf("      %2.3f     ", value)
+    end if input
+    printf "\n\n"
+    @hidden_layer.print
+    printf "\n"
+    @output_layer.print
+
+    p @output_y
+    p TRUE_OUTPUT[type]
+    printf "\n\n"
+    sleep 0.1
+  end
+
+  private
+
+  def calculate_output_delta output, type
+    Vector.elements(TRUE_OUTPUT[type]) - Vector.elements(output)
+  end
+
+  def round_values values
+    values.map { |value| value.round }
+  end
+
+  def check_is_output_correct type
+    calculate_output_delta(@output_y, type).each { |i| return 0 if i != 0 }
+    1
+  end
+end
+
 data = DataSet.new
-n.learn_by data.values
-n.learn_by data.values
-n.learn_by data.values
-n.learn_by data.values
-n.print_results data.values
+
+
+# n = Neuron.new
+# n.learn_by data.values
+# n.learn_by data.values
+# n.learn_by data.values
+# n.learn_by data.values
+# n.print_results data.values
+p = Perceptron.new 4, 3
+i = 0
+loop do
+  i += 1
+  num_of_correct_results = p.learn_by_set(data.values)
+  persent = num_of_correct_results / ((LEARNING_SET_END - LEARNING_SET_START) * 3.0)
+  puts "Persent of correct outputs is: #{(persent * 100).round}%"
+  break if persent >= LEARNING_TRESHOLD
+end
+
+puts "\nIterations count is: #{i}"
+
+
 
 
 
@@ -80,5 +223,4 @@ def count
   v = Vector[5.9799999999999995, 2.7, 4.279999999999999, 1.3]
   sum = ((s.magnitude)**2 - (v.magnitude)**2) / 2
   printf("%f*x1 + %f*x2 + %f*x3 + %f*x4 + %f", s[0] - v[0], s[1] - v[1], s[2] - v[2], s[3] - v[3], sum)
-
 end
