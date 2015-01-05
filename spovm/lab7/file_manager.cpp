f1de23806188173c299b98ff996a585c6f26a1a8
evheny0 <evheny0@gmail.com>
2015-01-05 18:54:58 +0300

    spovm7: completed file manager

--- spovm/lab7/file_manager.cpp
+++ spovm/lab7/file_manager.cpp
@@ -16,30 +16,19 @@ FileManager::~FileManager()
 
 void FileManager::start()
 {
-    slfs::File f1("aa/file");
-    slfs::File f2("bbb/file");
-    slfs::File f4("ccc/file");
-    slfs::File f5("ddd/file");
-    slfs::File f6("eee/ccc/ggg/aaa");
-    slfs::File f7("hhh/alal");
+    // slfs::File f1("aa/file");
+    // slfs::File f2("bbb/file");
+    // slfs::File f4("ccc/file");
+    // slfs::File f5("ddd/file");
+    // slfs::File f6("eee/ccc/ggg/aaa");
+    // slfs::File f7("hhh/alal");
     bool is_run = true;
-    int i;
     slfs::Dir root(std::string("/")), parent_dir(std::string("/"));
     std::vector<File *> dirs = root.get_all_files_list();
     std::vector<File *>::iterator it, current = dirs.begin();
 
     while(is_run) {
-        i = 0;
-        for(it = dirs.begin(); it != dirs.end(); it++, i++) {
-            if (it == current) {
-                gr::set_color(A_UNDERLINE);
-            } else {
-                gr::set_color(A_NORMAL);
-            }
-            gr::print_to((**it).get_c_name(), 2, i);
-            print_type(*it, i);
-        }
-        // char sym = getchar();
+        print_file_list(dirs, current);
         switch (getchar()) {
         case 's': 
             if (++current == dirs.end()) {
@@ -53,17 +42,23 @@ void FileManager::start()
             break;
         case 'd':
             if ((**current).get_type() == DIR) {
-                parent_dir = *((Dir *) *current);
-                dirs = parent_dir.get_all_files_list();
-                current = dirs.begin();
-                clear();
+                goto_new_folder(current, parent_dir, dirs);
+            } else {
+                show_file(current);
             }
             break;
         case 'a':
-            parent_dir.open(parent_dir.get_parent_link());
+            goto_parent_folder(current, parent_dir, dirs);
+            break;
+        case 'o':
+            create_new_dir(parent_dir);
             dirs = parent_dir.get_all_files_list();
             current = dirs.begin();
-            clear();
+            break;
+        case 'i':
+            create_new_file(parent_dir);
+            dirs = parent_dir.get_all_files_list();
+            current = dirs.begin();
             break;
         case 27: 
             is_run = false; 
@@ -71,6 +66,47 @@ void FileManager::start()
         }
     }
 }
+void FileManager::goto_new_folder(std::vector<File *>::iterator &current, Dir &parent_dir, std::vector<File *> &dirs)
+{
+    if ((**current).get_type() == DIR) {
+        parent_dir = *((Dir *) *current);
+        dirs = parent_dir.get_all_files_list();
+        current = dirs.begin();
+        clear();
+    }
+}
+
+void FileManager::show_file(std::vector<File *>::iterator &current)
+{
+    clear();
+    gr::print_to((**current).read(), 1, 1);
+    getchar();
+    clear();
+}
+
+void FileManager::goto_parent_folder(std::vector<File *>::iterator &current, Dir &parent_dir, std::vector<File *> &dirs)
+{
+    parent_dir.open(parent_dir.get_parent_link());
+    dirs = parent_dir.get_all_files_list();
+    current = dirs.begin();
+    clear();
+}
+
+void FileManager::print_file_list(std::vector<File *> &dirs, std::vector<File *>::iterator &current)
+{
+    int i = 1;
+    std::vector<File *>::iterator it;
+    for(it = dirs.begin(); it != dirs.end(); it++, i++) {
+        if (it == current) {
+            gr::set_color(A_UNDERLINE);
+        } else {
+            gr::set_color(A_NORMAL);
+        }
+        gr::print_to((**it).get_c_name(), 2, i);
+        print_type(*it, i);
+    }
+}
+
 
 void FileManager::print_type(File *file, int pos)
 {
@@ -80,6 +116,24 @@ void FileManager::print_type(File *file, int pos)
         gr::print_to("File", 15, pos);
     }
 }
+
+void FileManager::create_new_dir(Dir &parent_dir)
+{
+    slfs::Dir new_dir;
+    // new_dir.create(std::string("i.filename"), parent_dir.get_info_link(), DIR);
+}
+
+void FileManager::create_new_file(Dir &parent_dir)
+{
+    slfs::File new_file;
+    std::string str;
+    std::cin >> str;
+    new_file.create(str, parent_dir.get_info_link(), FILE);
+    IFilesystem::fs_manager->add_file_to_dir(new_file.get_info_link(), parent_dir.get_info_link(), FILE);
+    std::cin >> str;
+    new_file.write((char *)str.c_str());
+}
+
 
 
 } // slfs
--- spovm/lab7/file_manager.h
+++ spovm/lab7/file_manager.h
@@ -14,6 +14,13 @@ class FileManager {
     ~FileManager();
     void start();
     void print_type(File *file, int pos);
+    void goto_new_folder(std::vector<File *>::iterator &current, Dir &parent_dir, std::vector<File *> &dirs);
+    void goto_parent_folder(std::vector<File *>::iterator &current, Dir &parent_dir, std::vector<File *> &dirs);
+    void print_file_list(std::vector<File *> &dirs, std::vector<File *>::iterator &current);
+    void create_new_dir(Dir &parent_dir);
+    void create_new_file(Dir &parent_dir);
+    std::string enter_string();
+    void show_file(std::vector<File *>::iterator &current);
 };
 
 
